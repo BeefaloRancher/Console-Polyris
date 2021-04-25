2dd572fff36b522a8dab8e64eba5f07815122d69
BeefaloRancher <matthew.kidd60@gmail.com>
2021-04-25 17:00:35 +0100

    Updated abstact Polyomino class.

--- Polyris/Polyris/Polyomino/Polyomino.cs
+++ Polyris/Polyris/Polyomino/Polyomino.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Polyris.Polyomino
 {
-    class Polyomino
+    /// <summary>
+    /// This abstract polyomino class defines the necessary methods and properties that all inherited polyomino classes must
+    /// have to function in this game. All polyomino subclasses should inherit this class.
+    /// </summary>
+    public abstract class Polyomino
     {
+        internal char[,] peice;
+
+        /// <summary>
+        /// The rotate method takes in a polyomino peice represented by a 2D char array and rotates it 90 degrees clockwise.
+        /// </summary>
+        public void Rotate(char[,] peice) 
+        {
+            //TODO: Use techniques from algorithms and data structures book to figure out the process of rotating a 2D matrix .
+        }
+
     }
 }
